f6a6114a278bffb1457a312003b4e2e431e6878e
HarikaGogineni <84688515+HarikaGogineni@users.noreply.github.com>
2021-05-27 18:47:12 -0400

    Update web.tf

--- web.tf
+++ web.tf
@@ -3,3 +3,14 @@ provider "aws"{
     secret_key="iArf0i7kl5UQiwSbggJgBx7nrvrsIJ8rNoh8mHYv"
     region = "us-east-2"
 }
+resource "aws_instance" "myfirstInstance" {
+    ami  = "ami-00399ec92321828f5"
+    count = 1
+    key_name = "AWS_KeyPair"
+    instance_type = "t2.micro"
+    
+
+tags= {
+    Name = "aws_instance"
+}  
+}
